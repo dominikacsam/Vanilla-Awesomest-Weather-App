b89f7ea3caa65d6ccaa402f8d3fb1c71bdb26f05
dominikacsam <dominika@constructionsafety.ca>
2021-07-10 10:59:01 -0500

    Added forecast days using JS loop

--- src/hello.js
+++ src/hello.js
@@ -59,6 +59,36 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+
+              <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img
+                  src="http://openweathermap.org/img/wn/03d@2x.png"
+                  alt=""
+                  width="36"
+                />
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperature-max"> 18</span>
+                  <span class="weather-forecast-temperature-min"> 12</span>
+                </div>
+              </div>
+  
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function displayFaTemperature(event) {
   event.preventDefault();
 
@@ -76,6 +106,8 @@ function displayCeTemperature(event) {
   ceLink.classList.add("active");
   faLink.classList.remove("active");
 }
+
+displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
